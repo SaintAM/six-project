b0e549dcd9413f1201d4edab376368253edda4dd
SaintAM <mistersteepstar@gmail.com>
2025-10-19 18:32:05 +0300

    modal-coomit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.scss";
+import Modal from "./components/Modal";
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const onClickTogglModal = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickTogglModal} className="open-modal-btn">
+        ✨ Открыть окно
+      </button>
+
+      <Modal open={open} closeOpen={onClickTogglModal}>
+        <>
+          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+          <h4>Модальное окошко</h4>
+        </>
+      </Modal>
     </div>
   );
 }
